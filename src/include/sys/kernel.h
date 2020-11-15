19c9e192ad114d8fb35af494607cdc69e042196c
Mustafa Algayar <mustafa.algayar@protonmail.com>
2020-11-15 20:57:03 +0200

    add variadic macro for printing error messages and halting the system on fatal errors

--- src/include/sys/kernel.h
+++ src/include/sys/kernel.h
@@ -1,6 +1,15 @@
 #ifndef __KERNEL_H
 #define __KERNEL_H
 
+#include <printk.h>
+
+
+#define kernel_panic(...)    \
+    do {                     \
+    	printk(__VA_ARGS__); \
+    	hlt();               \
+    } while (0)
+
 /* halt the system */
 #define hlt()      \
     __asm__ (      \
